d1f712f0fa8fbfc70480b870c5c7095d915bea8c
Ayman <bsef18m021@pucit.edu.pk>
2021-04-03 23:47:53 +0500

    English Alphabets Interface

---
+++ Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
@@ -0,0 +1,14 @@
+package org.haqnawaz.learning_english_alphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlphabetsWithSounds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabets_with_sounds);
+    }
+}
--- Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/MainActivity.java
+++ Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Display(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        Intent intent = new Intent(MainActivity.this, AlphabetsWithSounds.class);
         startActivity(intent);
     }
 }
