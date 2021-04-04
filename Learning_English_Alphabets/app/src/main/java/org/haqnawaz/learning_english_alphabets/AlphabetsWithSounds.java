3bd1cfc027dcbeb72852c7c741d1036db082d925
Ayman <bsef18m021@pucit.edu.pk>
2021-04-04 09:10:23 +0500

    Animation On Alphabet Interface

--- Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
+++ Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
@@ -3,6 +3,8 @@ package org.haqnawaz.learning_english_alphabets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AlphabetsWithSounds extends AppCompatActivity {
 
@@ -11,4 +13,109 @@ public class AlphabetsWithSounds extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alphabets_with_sounds);
     }
+    public void AlphabetA(View view) {
+        ImageView imageView =findViewById(R.id.alphabetA);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetB(View view) {
+        ImageView imageView =findViewById(R.id.alphabetB);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetC(View view) {
+        ImageView imageView =findViewById(R.id.alphabetC);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetD(View view) {
+        ImageView imageView =findViewById(R.id.alphabetD);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetE(View view) {
+        ImageView imageView =findViewById(R.id.alphabetE);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetF(View view) {
+        ImageView imageView =findViewById(R.id.alphabetF);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetG(View view) {
+        ImageView imageView =findViewById(R.id.alphabetG);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetH(View view) {
+        ImageView imageView =findViewById(R.id.alphabetH);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetI(View view) {
+        ImageView imageView =findViewById(R.id.alphabetI);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetJ(View view) {
+        ImageView imageView =findViewById(R.id.alphabetJ);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetK(View view) {
+        ImageView imageView =findViewById(R.id.alphabetK);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetL(View view) {
+        ImageView imageView =findViewById(R.id.alphabetL);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetM(View view) {
+        ImageView imageView =findViewById(R.id.alphabetM);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetN(View view) {
+        ImageView imageView =findViewById(R.id.alphabetN);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetO(View view) {
+        ImageView imageView =findViewById(R.id.alphabetO);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetP(View view) {
+        ImageView imageView =findViewById(R.id.alphabetP);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetQ(View view) {
+        ImageView imageView =findViewById(R.id.alphabetQ);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetR(View view) {
+        ImageView imageView =findViewById(R.id.alphabetR);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetS(View view) {
+        ImageView imageView =findViewById(R.id.alphabetS);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetT(View view) {
+        ImageView imageView =findViewById(R.id.alphabetT);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetU(View view) {
+        ImageView imageView =findViewById(R.id.alphabetU);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetV(View view) {
+        ImageView imageView =findViewById(R.id.alphabetV);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetW(View view) {
+        ImageView imageView =findViewById(R.id.alphabetW);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetX(View view) {
+        ImageView imageView =findViewById(R.id.alphabetX);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetY(View view) {
+        ImageView imageView =findViewById(R.id.alphabetY);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+    public void AlphabetZ(View view) {
+        ImageView imageView =findViewById(R.id.alphabetZ);
+        imageView.animate().rotationBy(360).setDuration(2000);
+    }
+
 }
