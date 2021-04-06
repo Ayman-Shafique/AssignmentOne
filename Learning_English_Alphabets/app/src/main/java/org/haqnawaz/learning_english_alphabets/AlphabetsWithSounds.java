c0058605d4cb9d3ba682203f6c2910df084df528
Ayman <bsef18m021@pucit.edu.pk>
2021-04-06 23:56:23 +0500

    Rotation Animation and Audio Animation is used in interface

--- Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
+++ Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
@@ -21,81 +21,133 @@ public class AlphabetsWithSounds extends AppCompatActivity {
     }
     public void AlphabetA(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.a_sound);
+        mp.start();
     }
     public void AlphabetB(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.b_sound);
+        mp.start();
     }
     public void AlphabetC(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.c_sound);
+        mp.start();
     }
     public void AlphabetD(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.d_sound);
+        mp.start();
     }
     public void AlphabetE(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.e_sound);
+        mp.start();
     }
     public void AlphabetF(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.g_sound);
+        mp.start();
     }
     public void AlphabetG(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.g_sound);
+        mp.start();
     }
     public void AlphabetH(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.h_sound);
+        mp.start();
     }
     public void AlphabetI(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.i_sound);
+        mp.start();
     }
     public void AlphabetJ(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.j_sound);
+        mp.start();
     }
     public void AlphabetK(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.k_sound);
+        mp.start();
     }
     public void AlphabetL(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.l_sound);
+        mp.start();
     }
     public void AlphabetM(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.m_sound);
+        mp.start();
     }
     public void AlphabetN(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.n_sound);
+        mp.start();
     }
     public void AlphabetO(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.o_sound);
+        mp.start();
     }
     public void AlphabetP(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.p_sound);
+        mp.start();
     }
     public void AlphabetQ(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.q_sound);
+        mp.start();
     }
     public void AlphabetR(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.r_sound);
+        mp.start();
     }
     public void AlphabetS(View view) {
         ShowRotation(view);
+        //mp=MediaPlayer.create(this,R.raw.s_sound);
+        //mp.start();
     }
     public void AlphabetT(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.t_sound);
+        mp.start();
     }
     public void AlphabetU(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.u_sound);
+        mp.start();
     }
     public void AlphabetV(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.v_sound);
+        mp.start();
     }
     public void AlphabetW(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.w_sound);
+        mp.start();
     }
     public void AlphabetX(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.x_sound);
+        mp.start();
     }
     public void AlphabetY(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.y_sound);
+        mp.start();
     }
     public void AlphabetZ(View view) {
         ShowRotation(view);
+        mp=MediaPlayer.create(this,R.raw.z_sound);
+        mp.start();
     }
 
 }
