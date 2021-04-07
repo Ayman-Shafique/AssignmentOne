7f59e6bc8533e29f89e9611706c26edb92377533
Ayman <bsef18m021@pucit.edu.pk>
2021-04-08 01:25:55 +0500

    Final Assignment For Submission

--- Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
+++ Learning_English_Alphabets/app/src/main/java/org/haqnawaz/learning_english_alphabets/AlphabetsWithSounds.java
@@ -19,135 +19,89 @@ public class AlphabetsWithSounds extends AppCompatActivity {
         ImageView imageView = findViewById(view.getId());
         imageView.animate().rotationBy(360).setDuration(1500);
     }
-    public void AlphabetA(View view) {
+    public void AlphabetSounds(View view) {
         ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.a_sound);
+        switch (view.getId())
+        {
+            case R.id.alphabetA:
+                mp = MediaPlayer.create(this,R.raw.a_sound);
+                break;
+            case R.id.alphabetB:
+                mp = MediaPlayer.create(this,R.raw.b_sound);
+                break;
+            case R.id.alphabetC:
+                mp = MediaPlayer.create(this,R.raw.c_sound);
+                break;
+            case R.id.alphabetD:
+                mp = MediaPlayer.create(this,R.raw.d_sound);
+                break;
+            case R.id.alphabetE:
+                mp = MediaPlayer.create(this,R.raw.e_sound);
+                break;
+            case R.id.alphabetF:
+                mp = MediaPlayer.create(this,R.raw.f_sound);
+                break;
+            case R.id.alphabetG:
+                mp = MediaPlayer.create(this,R.raw.g_sound);
+                break;
+            case R.id.alphabetH:
+                mp = MediaPlayer.create(this,R.raw.h_sound);
+                break;
+            case R.id.alphabetI:
+                mp = MediaPlayer.create(this,R.raw.i_sound);
+                break;
+            case R.id.alphabetJ:
+                mp = MediaPlayer.create(this,R.raw.j_sound);
+                break;
+            case R.id.alphabetK:
+                mp = MediaPlayer.create(this,R.raw.k_sound);
+                break;
+            case R.id.alphabetL:
+                mp = MediaPlayer.create(this,R.raw.l_sound);
+                break;
+            case R.id.alphabetM:
+                mp = MediaPlayer.create(this,R.raw.m_sound);
+                break;
+            case R.id.alphabetN:
+                mp = MediaPlayer.create(this,R.raw.n_sound);
+                break;
+            case R.id.alphabetO:
+                mp = MediaPlayer.create(this,R.raw.o_sound);
+                break;
+            case R.id.alphabetP:
+                mp = MediaPlayer.create(this,R.raw.p_sound);
+                break;
+            case R.id.alphabetQ:
+                mp = MediaPlayer.create(this,R.raw.q_sound);
+                break;
+            case R.id.alphabetR:
+                mp = MediaPlayer.create(this,R.raw.r_sound);
+                break;
+            case R.id.alphabetS:
+                mp = MediaPlayer.create(this,R.raw.s_sound);
+                break;
+            case R.id.alphabetT:
+                mp = MediaPlayer.create(this,R.raw.t_sound);
+                break;
+            case R.id.alphabetU:
+                mp = MediaPlayer.create(this,R.raw.u_sound);
+                break;
+            case R.id.alphabetV:
+                mp = MediaPlayer.create(this,R.raw.v_sound);
+                break;
+            case R.id.alphabetW:
+                mp = MediaPlayer.create(this,R.raw.w_sound);
+                break;
+            case R.id.alphabetX:
+                mp = MediaPlayer.create(this,R.raw.x_sound);
+                break;
+            case R.id.alphabetY:
+                mp = MediaPlayer.create(this,R.raw.y_sound);
+                break;
+            case R.id.alphabetZ:
+                mp = MediaPlayer.create(this,R.raw.z_sound);
+                break;
+        }
         mp.start();
     }
-    public void AlphabetB(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.b_sound);
-        mp.start();
-    }
-    public void AlphabetC(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.c_sound);
-        mp.start();
-    }
-    public void AlphabetD(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.d_sound);
-        mp.start();
-    }
-    public void AlphabetE(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.e_sound);
-        mp.start();
-    }
-    public void AlphabetF(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.f_sound);
-        mp.start();
-    }
-    public void AlphabetG(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.g_sound);
-        mp.start();
-    }
-    public void AlphabetH(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.h_sound);
-        mp.start();
-    }
-    public void AlphabetI(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.i_sound);
-        mp.start();
-    }
-    public void AlphabetJ(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.j_sound);
-        mp.start();
-    }
-    public void AlphabetK(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.k_sound);
-        mp.start();
-    }
-    public void AlphabetL(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.l_sound);
-        mp.start();
-    }
-    public void AlphabetM(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.m_sound);
-        mp.start();
-    }
-    public void AlphabetN(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.n_sound);
-        mp.start();
-    }
-    public void AlphabetO(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.o_sound);
-        mp.start();
-    }
-    public void AlphabetP(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.p_sound);
-        mp.start();
-    }
-    public void AlphabetQ(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.q_sound);
-        mp.start();
-    }
-    public void AlphabetR(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.r_sound);
-        mp.start();
-    }
-    public void AlphabetS(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.s_sound);
-        mp.start();
-    }
-    public void AlphabetT(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.t_sound);
-        mp.start();
-    }
-    public void AlphabetU(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.u_sound);
-        mp.start();
-    }
-    public void AlphabetV(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.v_sound);
-        mp.start();
-    }
-    public void AlphabetW(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.w_sound);
-        mp.start();
-    }
-    public void AlphabetX(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.x_sound);
-        mp.start();
-    }
-    public void AlphabetY(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.y_sound);
-        mp.start();
-    }
-    public void AlphabetZ(View view) {
-        ShowRotation(view);
-        mp=MediaPlayer.create(this,R.raw.z_sound);
-        mp.start();
-    }
-
 }
